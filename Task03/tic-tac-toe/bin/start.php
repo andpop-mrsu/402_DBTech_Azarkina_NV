8cb12feb947374f7cf60fe5922a0c2eda374652e
Natalia Azarkina <azarkinanat@mail.ru>
2023-10-15 17:36:00 +0300

    v2.0

---
+++ Task03/tic-tac-toe/bin/start.php
@@ -0,0 +1,19 @@
+#!/usr/bin/env php
+
+<?php
+
+$pathForGithub = __DIR__.'/../vendor/autoload.php';
+$pathForPackagist = __DIR__.'/../../../autoload.php';
+
+if (file_exists($pathForGithub)) {
+    require_once($pathForGithub);
+} else {
+    require_once($pathForPackagist);
+}
+
+// require_once __DIR__ . '/../vendor/autoload.php';
+
+use PUMUSHKA\TicTacToe\Controller\Controller;
+
+$controller = new Controller();
+$controller->startGame();
